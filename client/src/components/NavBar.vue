dbcc9e9501e1afea0bb2548f19367d9ec9258fd3
williamxmejia <willx1995@gmail.com>
2023-05-10 11:47:59 -0500

    feat: created nav bar and removed set up file

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul class="nav-list">
+            <li class="nav-item">Home</li>
+            <li class="nav-item">Profile</li>
+            <li class="nav-item">Workouts</li>
+            <li class="nav-item">Life style</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .nav-list {
+        display: flex;
+        list-style: none;
+
+        justify-content: center;
+        /* margin: 20px; */
+    }
+
+    .nav-item {
+        margin: 20px;
+    }
+</style>
